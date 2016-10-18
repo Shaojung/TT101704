cb0c667e9ddd311ca44a4afbc723caf2ada3c939
Shaojung <shaojung@gmail.com>
2016-10-18 09:44:24 +0800

    Broadcast receiver and notification

--- app/src/main/java/com/test/tt101704/MyReceiver.java
+++ app/src/main/java/com/test/tt101704/MyReceiver.java
@@ -1,9 +1,14 @@
 package com.test.tt101704;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class MyReceiver extends BroadcastReceiver {
     public MyReceiver() {
@@ -14,5 +19,23 @@ public class MyReceiver extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         Log.d("mylog", "已經收到廣播");
+        NotificationManager nm = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        Intent it = new Intent(context, MainActivity.class);
+        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP |
+                Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        int requestCode = 1;
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                requestCode, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentTitle("Test1")
+                .setContentText("倒數計時結束")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        Notification notification = builder.build();
+        nm.notify(123, notification);
+
     }
 }
